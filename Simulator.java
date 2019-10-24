b15dc0def86032bb6a358eb4706e04f2247d5896
rdj519 <39096856+rdj519@users.noreply.github.com>
2019-10-24 14:04:39 +0800

    Add files via upload

--- Simulator.java
+++ Simulator.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /* The chosen algorithm will be implemented here, as well as the simulation, and counting of moves etc.*/
 
 
@@ -9,12 +9,55 @@ public class Simulator
 {
 	private Bot bot;
 	private Maze maze;
-	
-	/* where n is the grid */
-	public Simulator(int n) 
+
+		
+	public void run()
 	{
+		Scanner sc = new Scanner(System.in);
+		int x, y, nSize;
+		String input;
+		int nSteps = 0;
+		
+		System.out.println("Enter size: ");
+		input = sc.nextLine();
+		nSize = Integer.parseInt(input);
+		
 		bot = new Bot();
-		maze = new Maze(n, bot); //it means that the bot is already in the starting state
+		maze = new Maze(nSize, bot); //it means that the bot is already in the starting state
+		
+		maze.display();
+		System.out.print("\n\nEnter coordinates for barricading (-1 to stop): ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		try 
+		{
+			while(x != -1 && y != -1)
+			{
+				maze.wallify(x, y);
+				maze.display();
+				x = sc.nextInt();
+				y = sc.nextInt();
+			}
+		}
+		catch(Exception e)
+		{
+			
+		}
+
+		
+		System.out.print("Press anything to start simulation\n ");
+		while(maze.isFinished() == false)
+		{
+				maze.moveForward();
+				nSteps++;
+				maze.display(); 
+			
+		}
+		
+		System.out.println("Steps: " + nSteps);
+		
+		
+		sc.close();
 	}
 
 }
